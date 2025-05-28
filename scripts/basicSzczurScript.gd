c661c3c851b168c59d8a1119146dfe9f8b53b8fc
MaciekMaslanka <maciek.maslanka@o2.pl>
2025-05-28 21:19:12 +0200

    dodano parę rzeczy
    nwm jakie XD

---
+++ scripts/basicSzczurScript.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+@onready var leftEye = $Sprite2D/LeftEye
+@onready var rightEye = $Sprite2D/RightEye
+@export var maxEyeOffset: float = 5;
+@export var eyeFollowingSpeed: float = 0.5;
+var leftBasePos = Vector2.ZERO
+var rightBasePos = Vector2.ZERO
+
+@export var speed = 100;
+@export var rotationFromWalls = 0.5;
+@export var rotationToPlayer:float = 0
+@onready var player = get_node("/root/Node2D/Player") #pobranie nodea gracza
+
+var direction = Vector2.ZERO
+
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	direction = Vector2.RIGHT.rotated(deg_to_rad(rng.randi_range(0, 360)))
+	
+	leftBasePos = leftEye.position
+	rightBasePos = rightEye.position
+	
+func _physics_process(delta: float) -> void:
+	setRotationToPlayer(delta)
+	direction = direction.normalized() * speed
+	setRotation()
+	updateEyes()
+	
+	var collision = move_and_collide(direction * delta)
+	if collision:
+		var normal = collision.get_normal();
+		direction = direction.bounce(normal)
+	
+func setRotation(): #ustawia poprawną rotację + zmienia kierunek delikatnie w stronę gracza
+	var targetRotation = direction.angle()
+	rotation = lerp_angle(rotation, targetRotation, rotationFromWalls)
+	
+func setRotationToPlayer(delta):
+	var vectorToPlayer = (player.global_position - global_position).normalized() #oblicza wektor do gracza (trzeba było słuchać na matmie)
+	var angleToPlayer = vectorToPlayer.angle()
+	var currentAngle = direction.angle()
+	var newAngle = lerp_angle(currentAngle, angleToPlayer, rotationToPlayer * delta);
+	direction = Vector2.RIGHT.rotated(newAngle)
+	
+func updateEyes():
+	var toPlayer = (player.global_position - global_position).normalized()
+	toPlayer = to_local(global_position + toPlayer)
+	var eyeOffset = toPlayer * maxEyeOffset
+	
+	leftEye.position = leftEye.position.lerp(leftBasePos + eyeOffset, eyeFollowingSpeed)
+	rightEye.position = rightEye.position.lerp(rightBasePos + eyeOffset, eyeFollowingSpeed)
